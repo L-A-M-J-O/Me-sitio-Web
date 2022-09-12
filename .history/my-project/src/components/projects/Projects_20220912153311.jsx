3a8701301afa6f43da644a4e56f51aec167937ba
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-09-12 15:35:14 +0200

    Iniciando la creacion de Sitio Web

---
+++ .history/my-project/src/components/projects/Projects_20220912153311.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div className='project'>
+        <p class="text-sm ...">The quick brown fox ...</p>
+        <p class="text-base ...">The quick brown fox ...</p>
+        <p class="text-lg ...">The quick brown fox ...</p>
+        <p class="text-xl ...">The quick brown fox ...</p>
+        <p class="text-2xl ...">The quick brown fox ...</p>
+    </div>
+  )
+}
+
+export default Projects
---
+++ .history/my-project/src/components/projects/Projects_20220912153438.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div className='project'>
+        <div class="columns-3">
+            <p class="text-2xl">The quick brown fox ...</p>
+            <p class="text-2xl ...">The quick brown fox ...</p>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
