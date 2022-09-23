1a4391a9b007a9404540bbf2569fee3094a84538
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-09-23 21:30:46 +0200

    Cambios en Me skills

---
+++ .history/my-project/src/App_20220923195052.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+import Home from './components/Home/Home';
+import Job from './components/Job/Job'
+import Me from './components/Me/Me'
+
+function App() {
+  return (
+    <div className='main'>
+      <Home/>
+      <section>
+        <Job/>
+    </section>
+      <Me/>
+    </div>
+
+
+  );
+}
+
+export default App; 
