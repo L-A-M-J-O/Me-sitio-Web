7649232b61e46f841395092b0465baf557bffd23
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-09-30 17:24:18 +0200

    Quitando history

--- .history/my-project/src/components/About/About_20220915154804.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function About() {
-  return (
-    <div>About, HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE</div>
-  )
-}
-
-export default About
--- .history/my-project/src/components/Job/Job_20220919163356.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Job() {
-  return (
-    <div id='job'>Job</div>
-  )
-}
-
-export default Job
