62a8057975a5b92fd67382591eba29e2927d0d22
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-10-02 16:48:28 +0200

    Cambios la tarjeta de Me

--- my-project/src/components/Me/Me.jsx
+++ my-project/src/components/Me/Me.jsx
@@ -1,30 +1,19 @@
 import '../Me/Me.css';
 
-import me from '../../assets/img/me1.jpg'
-
 function Me() {
   return (
     <div id='about'>
-      <div className='row justify-content-start me-0 m-5'>
-        <div className='col-10'>
-          <div className='row hero-me justify-content-center mb-5'>
-            <div className='col-12 col-sm-12 col-md-7 col-lg-7'>
-              <div className="card">
-                <div className='row justify-content-center'>
-                  <div className='col-9'>
-                    <div className="card-body text-center">
-                      <h5 className="card-title">Front End Engineer &#x2614;</h5>
-                      <h6 className="card-subtitle mb-2 text-muted">Alan Mateo Naranjo Alvarado</h6>
-                      <p className='text-start text-md-center text-lg-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eveniet porro unde incidunt nemo suscipit, non est accusantium magni possimus voluptatibus a praesentium fugiat sed alias debitis ratione voluptas mollitia facilis dolore repellendus? Illum fugiat temporibus quidem laborum voluptas modi voluptatum. Vitae ea corporis quisquam nobis blanditiis, dolore perferendis maxime?</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='col-10 col-sm-10 col-md-4 col-lg-4'>
-             <div className='box-img'>
-                <img src={me} alt='me'/>
-              </div>
+      <div className='container'>
+        <div className='row g-2 justify-content-center mt-5'>
+          <div className='col-12 col-sm-12 col-md-6 col-lg-6 m-5 text-center m-me-img'>
+            <div className='me-img'>
+            </div>  
+          </div>
+          <div className='col-10 col-sm-12 col-md-6 col-lg-6 align-self-center'>
+            <div className='box-hero-me row row-cols-1 justify-content-center'>
+              <div className='col-10 text-center'><h4 className='m-me-a'>Alan Mateo Naranjo Alvarado</h4></div>
+              <div className='col-8 text-center text-muted'><p className='m-me-i'>Front End Engineer && Athlete</p></div>
+              <div className='col text-center text-md-end text-lg-end'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus est debitis hic. Ab maiores quo voluptates fuga illo modi distinctio dignissimos cupiditate consequatur quas! Repellendus labore repudiandae laborum fuga mollitia beatae voluptate atque at aliquid reprehenderit! Quo quisquam perferendis consequuntur.</div>
             </div>
           </div>
         </div>
