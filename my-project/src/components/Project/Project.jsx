33b5b38d1e53145444b3c5ed2569c59f9b26147b
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-10-02 18:19:33 +0200

    Guardando cambios

--- my-project/src/components/Project/Project.jsx
+++ my-project/src/components/Project/Project.jsx
@@ -29,14 +29,21 @@ export default function App() {
           <div className='row'>
             <div className='col-12'>
               <div className="card-project">
+              <div className='row'>
+                <div className='col-12 col-sm-12 col-md-4 col-lg-4'>
+                  <img className='img-projects' src='https://img.freepik.com/free-psd/e-learning-landing-page-template-design_23-2149118530.jpg?w=2000' alt='projects'/>
+                </div>
+                <div className='col-12 col-sm-12 col-md-8 col-lg-8'>
                 <div className="card-body">
-                  <h5 className="card-title">React Ecommerce Website</h5>
+                  <h5 className="card-title">Reat Ecommerce Website</h5>
                   <h6 className="card-subtitle mb-2 text-muted">Html5, Css3, Bootstrap V5, JavaScript</h6>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                   <a href='https://es.backbone.digital/' className='media-icon-arrow'>
                     <button className='btn-project'><p>Go to see</p></button>        
                   </a>
                 </div>
+                </div>
+              </div>
               </div>
             </div>
           </div>
@@ -45,14 +52,21 @@ export default function App() {
           <div className='row'>
             <div className='col-12'>
               <div className="card-project">
+              <div className='row'>
+                <div className='col-12 col-sm-12 col-md-4 col-lg-4'>
+                  <img className='img-projects' src='https://img.freepik.com/free-psd/e-learning-landing-page-template-design_23-2149118530.jpg?w=2000' alt='projects'/>
+                </div>
+                <div className='col-12 col-sm-12 col-md-8 col-lg-8'>
                 <div className="card-body">
-                  <h5 className="card-title">Lading Page</h5>
+                  <h5 className="card-title">Reat Ecommerce Website</h5>
                   <h6 className="card-subtitle mb-2 text-muted">Html5, Css3, Bootstrap V5, JavaScript</h6>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href='https://ladign-3c53c.web.app/#' className='media-icon-arrow'>
+                  <a href='https://es.backbone.digital/' className='media-icon-arrow'>
                     <button className='btn-project'><p>Go to see</p></button>        
                   </a>
                 </div>
+                </div>
+              </div>
               </div>
             </div>
           </div>
@@ -61,14 +75,21 @@ export default function App() {
           <div className='row'>
             <div className='col-12'>
               <div className="card-project">
+              <div className='row'>
+                <div className='col-12 col-sm-12 col-md-4 col-lg-4'>
+                  <img className='img-projects' src='https://img.freepik.com/free-psd/e-learning-landing-page-template-design_23-2149118530.jpg?w=2000' alt='projects'/>
+                </div>
+                <div className='col-12 col-sm-12 col-md-8 col-lg-8'>
                 <div className="card-body">
-                  <h5 className="card-title">JavaScript knowledge</h5>
+                  <h5 className="card-title">Reat Ecommerce Website</h5>
                   <h6 className="card-subtitle mb-2 text-muted">Html5, Css3, Bootstrap V5, JavaScript</h6>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                   <a href='https://es.backbone.digital/' className='media-icon-arrow'>
                     <button className='btn-project'><p>Go to see</p></button>        
                   </a>
                 </div>
+                </div>
+              </div>
               </div>
             </div>
           </div>
@@ -77,14 +98,21 @@ export default function App() {
           <div className='row'>
             <div className='col-12'>
               <div className="card-project">
+              <div className='row'>
+                <div className='col-12 col-sm-12 col-md-4 col-lg-4'>
+                  <img className='img-projects' src='https://img.freepik.com/free-psd/e-learning-landing-page-template-design_23-2149118530.jpg?w=2000' alt='projects'/>
+                </div>
+                <div className='col-12 col-sm-12 col-md-8 col-lg-8'>
                 <div className="card-body">
-                  <h5 className="card-title">React Ecommerce Website</h5>
+                  <h5 className="card-title">Reat Ecommerce Website</h5>
                   <h6 className="card-subtitle mb-2 text-muted">Html5, Css3, Bootstrap V5, JavaScript</h6>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                   <a href='https://es.backbone.digital/' className='media-icon-arrow'>
                     <button className='btn-project'><p>Go to see</p></button>        
                   </a>
                 </div>
+                </div>
+              </div>
               </div>
             </div>
           </div>
@@ -93,14 +121,21 @@ export default function App() {
           <div className='row'>
             <div className='col-12'>
               <div className="card-project">
+              <div className='row'>
+                <div className='col-12 col-sm-12 col-md-4 col-lg-4'>
+                  <img className='img-projects' src='https://img.freepik.com/free-psd/e-learning-landing-page-template-design_23-2149118530.jpg?w=2000' alt='projects'/>
+                </div>
+                <div className='col-12 col-sm-12 col-md-8 col-lg-8'>
                 <div className="card-body">
-                  <h5 className="card-title">Java knowledge</h5>
+                  <h5 className="card-title">Reat Ecommerce Website</h5>
                   <h6 className="card-subtitle mb-2 text-muted">Html5, Css3, Bootstrap V5, JavaScript</h6>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                   <a href='https://es.backbone.digital/' className='media-icon-arrow'>
                     <button className='btn-project'><p>Go to see</p></button>        
                   </a>
                 </div>
+                </div>
+              </div>
               </div>
             </div>
           </div>
